b8dd97ba82c177ff07b72a8abd46e1116e692df3
sam bacha <sam@manifoldfinance.com>
2021-12-27 22:48:25 -0800

    feat(release): v0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+/** 
+ * 
+ * @package MajorDOMO SDK
+ * @version 0.1.0
+ * @license Apache-2.0
+ * 
+ */
+
+export * from './DictatorDAO';
+// @ts-ignore
+export * from './IERC20';
+
+
+export * from './factories/IERC20__factory'
+//export * from './factories/DictatorDAO__factory'
+
+
+// import { DictatorDAO } from './../../staking/contracts/types/DictatorDAO.d';
+/** @exports majordomo-sdk */
